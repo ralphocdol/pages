5c8a7a6135739dad0c8245718fd2123861949ca6
ralphocdol <ralph.ocdol63@gmail.com>
2020-08-02 15:19:12 +0800

    favicon.ico

--- src/App.js
+++ src/App.js
@@ -6,21 +6,30 @@ import ralphocdol from './ralphocdol.jpg';
 import './App.css';
 
 function App() {
+  const imgStyles = {
+    borderRadius: '50%',
+    width: '300px',
+    height: '300px',
+    border: '8px solid #fff',
+  };
+  const linkSpaces = {
+    marginLeft: '5px',
+    marginRight: '5px',
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={ralphocdol} alt="logo" style={{ 'border-radius': '50%', width: '300px', border: '8px solid #fff' }} draggable="false" />
-        {/*  className="App-logo" */}
+        <img src={ralphocdol} alt="logo" style={imgStyles} draggable="false" />
         <h2>Ralph Ocdol</h2>
         <div>
-          <a href="https://www.linkedin.com/in/ralphocdol/" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={linkedin} style={{ margin: '5' }} />
+          <a href="https://www.linkedin.com/in/ralphocdol/" target="_blank" rel="noopener noreferrer" style={linkSpaces}>
+            <FontAwesomeIcon icon={linkedin} />
           </a>
-          <a href="https://github.com/ralphocdol/" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={github} style={{ margin: '5' }} />
+          <a href="https://github.com/ralphocdol/" target="_blank" rel="noopener noreferrer" style={linkSpaces}>
+            <FontAwesomeIcon icon={github} />
           </a>
-          <a href="mailto:ralph.ocdol63@gmail.com">
-            <FontAwesomeIcon icon={email} style={{ margin: '5' }} />
+          <a href="mailto:ralph.ocdol63@gmail.com" style={linkSpaces}>
+            <FontAwesomeIcon icon={email} />
           </a>
         </div>
       </header>
